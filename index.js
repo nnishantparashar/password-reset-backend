33ee753f7a87b0c2218ea201b7617ac888f82b50
nishant parashar <nnishant.parashar@gmail.com>
2024-06-06 18:18:43 +0530

    index.js modified

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ db();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://peaceful-belekoy-8a3733.netlify.app/"); 
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use(authRoutes);
 
 
